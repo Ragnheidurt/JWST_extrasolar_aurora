05d20d28a31f724595178cb2f01adbb53bbafb08
Ragnheidur Tryggvadóttir <ragnheidurt3@gmail.com>
2024-02-19 11:29:09 +0100

    Can kinda do figures

--- Kirk_and_Spock/KirkSpock_firstData.py
+++ Kirk_and_Spock/KirkSpock_firstData.py
@@ -20,9 +20,9 @@ nrs1_header = fits.getheader(nrs1,ext=1)
 nrs2_header = fits.getheader(nrs2,ext=1)
 
 # Get the start and end wavelengths for both sensors
-nrs1_wavestart = nrs1_header['WAVESTART']
+nrs1_wavestart = nrs1_header['WAVSTART']
 nrs1_wavend = nrs1_header['WAVEND']
-nrs2_wavestart = nrs2_header['WAVESTART']
+nrs2_wavestart = nrs2_header['WAVSTART']
 nrs2_wavend = nrs2_header['WAVEND']
 
 # Calculate the interval and size of wavelength data
@@ -35,16 +35,27 @@ nrs2_interval = nrs2_dist/nrs2_data.shape[0]
 nrs1_wave = np.zeros(nrs1_data.shape[0])
 nrs2_wave = np.zeros(nrs2_data.shape[0])
 
-for i in nrs1_data.shape[0]:
+for i in range(nrs1_data.shape[0]):
     if i != 0: 
         nrs1_wave[i] = nrs1_wave[i-1] + nrs1_interval
     else:
         nrs1_wave[i] = nrs1_wavestart
 
-for i in nrs2_data.shape[0]:
+for i in range(nrs2_data.shape[0]):
     if i != 0:
         nrs2_wave[i] = nrs2_wave[i-1] + nrs2_interval
     else:
         nrs2_wave[i] = nrs2_wavestart
-        
+
+
+
+plt.figure(1)
+plt.imshow(nrs1_data[1500,:,:])
+plt.colorbar()
+
+plt.figure(2)
+plt.imshow(nrs2_data[1500,:,:])
+plt.colorbar()
+
+plt.show()
 
