Minor fix to KirkSpock, plot average for Firefly and fix wavelength

Firefly/Firefly_firstData.py
@@ -13,13 +13,12 @@ nrs2_data = fits.getdata(nrs2,ext=1)
 nrs1_header = fits.getheader(nrs1,ext=1)
 nrs2_header = fits.getheader(nrs2,ext=1)
 
-print(nrs1_header)
-print(nrs2_header)
-
+# The Wavestart and Wavend come from the header, but the gap is given for the slit S200A1 and G395H F290LP
 nrs1_wavestart = nrs1_header['WAVSTART']
-nrs1_wavend = nrs1_header['WAVEND']
-nrs2_wavestart = nrs2_header['WAVSTART']
+nrs1_wavend = 3.69e-6
+nrs2_wavestart = 3.79e-6
 nrs2_wavend = nrs2_header['WAVEND']
+
 
 # Calculate the interval and size of wavelength data
 nrs1_dist = nrs1_wavend-nrs1_wavestart
@@ -45,6 +44,13 @@ for i in range(nrs2_data.shape[0]):
     else:
         nrs2_wave[i] = nrs2_wavestart
 
+
+# Average all lines
+nrs1_allavg = np.nanmean(nrs1_data,axis=(1))
+nrs2_allavg = np.nanmean(nrs2_data,axis=(1))
+
+
+
 plt.figure(1)
 plt.imshow(nrs1_data)
 
@@ -52,6 +58,11 @@ plt.figure(2)
 plt.imshow(nrs2_data)
 
 plt.figure(3)
-plt.plot(nrs1_data)
+plt.plot(nrs1_wave,nrs1_data)
+plt.plot(nrs2_wave,nrs2_data)
+
+plt.figure(4)
+plt.plot(nrs1_wave,nrs1_allavg)
+plt.plot(nrs2_wave,nrs2_allavg)
 
 plt.show()

Kirk_and_Spock/KirkSpock_firstData.py
@@ -21,7 +21,7 @@ nrs2_data = fits.getdata(nrs2,ext=1)
 nrs1_header = fits.getheader(nrs1,ext=1)
 nrs2_header = fits.getheader(nrs2,ext=1)
 
-
+print(nrs1_header)
 # Get the start and end wavelengths for both sensors
 nrs1_wavestart = nrs1_header['WAVSTART']
 nrs1_wavend = nrs1_header['WAVEND']
@@ -37,6 +37,7 @@ nrs2_interval = nrs2_dist/nrs2_data.shape[0]
 # Create array with wavelength
 nrs1_wave = np.zeros(nrs1_data.shape[0])
 nrs2_wave = np.zeros(nrs2_data.shape[0])
+
 
 for i in range(nrs1_data.shape[0]):
     if i != 0: 