f7815a951def82731e83ba4e06cc3b944aa55322
Ragnheidur Tryggvadóttir <ragnheidurt3@gmail.com>
2024-05-07 17:13:10 +0200

    takes in both nrs1 and nrs2 i think

--- Functions/scale_model.py
+++ Functions/scale_model.py
@@ -1,12 +1,17 @@
 import numpy as np
 import bisect
 
-def scale_model(model, spectra, model_wave, spectra_wave):
+def scale_model(model, spectra1, spectra2, model_wave, spectra_wave):
     index = np.argmax(model)
     wave = model_wave[index]
     spectra_index_low = bisect.bisect_left(spectra_wave,model_wave[index-1])
     spectra_index_high = bisect.bisect_right(spectra_wave,model_wave[index+1])
-    sub_spectra = spectra[spectra_index_high:spectra_index_low]
+    if not np.all(np.isnan(spectra1[spectra_index_high:spectra_index_low])):
+        sub_spectra = spectra1[spectra_index_high:spectra_index_low]
+        spectra = spectra1
+    else:
+        sub_spectra = spectra2[spectra_index_high:spectra_index_low]
+        spectra = spectra2
     spectra_index = np.nanargmax(sub_spectra)
     spectra_index = spectra_index+spectra_index_high
     #if spectra_index != len(spectra_wave) and abs(spectra_wave[spectra_index]-wave)>abs(spectra_wave[spectra_index+1]-wave):
@@ -16,3 +21,13 @@ def scale_model(model, spectra, model_wave, spectra_wave):
     return scaled_model
 
 
+def scale_correctly(model, model_wave):
+    sorted_indices = np.argsort(model_wave)
+    sorted_wave = model_wave[sorted_indices]
+    sorted_model = model[sorted_indices]
+
+    wavelength_step = sorted_wave[1]-sorted_wave[0]
+    total_absorption = np.trapz(sorted_model,dx=wavelength_step)
+    scaling_factor = 10e16/total_absorption
+    scaled_model = sorted_model*scaling_factor
+    return scaled_model
